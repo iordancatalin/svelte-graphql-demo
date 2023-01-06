b923a0417e177b78439d38e0c5e29be0dd09b5e3
Catalin Iordan <catalin.iordan@uipath.com>
2023-01-06 16:48:51 +0200

    feat(client): create main pages

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,30 +1,99 @@
 <script lang="ts">
-	export let name: string;
+	import ActorsPage from "./pages/ActorsPage.svelte";
+	import MoviesPage from "./pages/MoviesPage.svelte";
+
+	let selectedTab: "Movies" | "Actors" = "Movies";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<section>
+	<header>
+		<button
+			on:click={() => (selectedTab = "Movies")}
+			class:selected={selectedTab === "Movies"}
+			class="tab-item"
+			type="button">Movies</button
+		>
+		<button
+			on:click={() => (selectedTab = "Actors")}
+			class:selected={selectedTab === "Actors"}
+			class="tab-item"
+			type="button">Actors</button
+		>
+	</header>
+
+	<main>
+		{#if selectedTab === "Movies"}
+			<MoviesPage />
+		{/if}
+
+		{#if selectedTab === "Actors"}
+			<ActorsPage />
+		{/if}
+	</main>
+</section>
+
+<svelte:head>
+	<style>
+		:root {
+			--background-color: #171717;
+			--color-secondary: #ededed;
+			--color-white: #ffffff;
+		}
+	</style>
+</svelte:head>
 
 <style>
+	:global(*) {
+		box-sizing: border-box;
+	}
+
+	:global(body) {
+		padding: 0;
+		min-height: 100vh;
+		background-color: var(--background-color);
+	}
+
+	section {
+		display: flex;
+		flex-direction: column;
+
+		height: 100%;
+		overflow: hidden;
+	}
+
+	header {
+		display: flex;
+		gap: 20px;
+		padding: 10px 20px;
+		border-bottom: 1px solid var(--color-secondary);
+	}
+
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+		display: flex;
+		flex: 1;
+
+		overflow: auto;
+		padding: 10px;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	.tab-item {
+		min-width: 150px;
+		padding: 10px;
+		transform: skewX(-20deg);
+		color: var(--color-secondary);
+		background-color: transparent;
+		border: 1px solid var(--color-secondary);
+		transition: backgorund-color 0.5s, color 0.5s;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.tab-item:hover {
+		cursor: pointer;
+		color: var(--background-color);
+		background-color: var(--color-secondary);
+	}
+
+	.tab-item.selected {
+		color: var(--background-color);
+		background-color: var(--color-secondary);
 	}
 </style>
---
+++ client/src/pages/MoviesPage.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<h1>This is the Movies page</h1>
+
+<style>
+    h1 {
+        height: 2000px;
+    }
+</style>
