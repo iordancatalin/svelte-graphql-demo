a7a64c7c3e2aa3d723dbe148bd73af2d5b2ecdca
Catalin Iordan <catalin.iordan@uipath.com>
2023-01-06 22:41:52 +0200

    chore: update main package.json

--- client/codegen.ts
+++ client/codegen.ts
@@ -1,7 +1,7 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-    schema: 'http:/localhost:4000',
+    schema: '../schema.graphql',
     documents: './src/**/*.gql',
     generates: {
         './src/graphql/generated.ts': {
